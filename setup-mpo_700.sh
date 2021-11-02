46d5d74344328907da15d095c981c765d7f1ed4e
Pradheep-office <padhupradheep@gmail.com>
2021-11-02 13:20:19 +0100

    adding setup script for mpo-700

---
+++ setup-mpo_700.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# exit if any command below fails
+set -e
+
+# Install navigation packages
+
+# Nav2
+sudo apt install -y ros-$ROS_DISTRO-navigation2 ros-$ROS_DISTRO-nav2-*
+
+sudo apt install -y ros-$ROS_DISTRO-slam-toolbox
+
+#Teleop-joy
+sudo apt-get install -y ros-$ROS_DISTRO-teleop-twist-joy
+
+#Teleop-key
+sudo apt-get install -y ros-$ROS_DISTRO-teleop-twist-keyboard
+
+rosdep update
+
+cd ~
+
+mkdir -p mpo_700_workspace/src
+cd mpo_700_workspace/src
+
+# clone git repos here...
+git clone --branch $ROS_DISTRO 	https://github.com/neobotix/neo_mpo_700-2.git
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_local_planner2.git
+git clone --branch master          https://github.com/neobotix/neo_common2
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_relayboard_v2-2
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_kinematics_omnidrive2.git
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_sick_s300-2
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_teleop2
+git clone --branch master          https://github.com/neobotix/neo_msgs2
+git clone --branch master          https://github.com/neobotix/neo_srvs2
+
+# build workspace
+cd ..
+colcon build --symlink-install 
+
+echo "export LC_NUMERIC="en_US.UTF-8" " >> ~/.bashrc
+
+echo "Installation successful !!!"
+
+exit 0
