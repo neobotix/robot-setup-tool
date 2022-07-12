d39eee20bc34a60e6193389ab0082a2a263fbf9d
Pradheep Krishna <padhupradheep@gmail.com>
2022-07-12 17:18:31 +0200

    Update setup-simulation.sh

--- package-setup/setup-simulation.sh
+++ package-setup/setup-simulation.sh
@@ -39,7 +39,7 @@ cd neobotix_workspace/src
 
 # clone git repos here...
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_simulation2.git
-git clone --branch main            https://github.com/neobotix/neo_nav2_bringup.git
+git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_nav2_bringup.git
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_local_planner2.git
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_localization2.git
 git clone --branch master          https://github.com/neobotix/neo_common2
