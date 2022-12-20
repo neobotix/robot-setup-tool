afab74008bf3a4340213b13f93b6c0c2644e6a2f
par <reiter@neobotix.de>
2022-12-20 22:11:35 +0100

    query user + clear history

--- debian/neobotix-tools/opt/neobotix/tools/cleanup.sh
+++ debian/neobotix-tools/opt/neobotix/tools/cleanup.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-username=neobotix
+set -e
+
+if [ $SUDO_USER ]; then user=$SUDO_USER; else user=`whoami`; fi
 
 # check for root privileges
 uid=$(/usr/bin/id -u) && [ "$uid" = "0" ] || { echo "Run this script as root!"; exit 1; }
 
 # empty trash
-rm -rf /home/$username/.local/share/Trash/*
+rm -rf /home/$user/.local/share/Trash/*
 
 # remove unused packages
 apt-get -y autoremove
 
 # delete ros logs
-rm -rf /home/$username/.ros/log/*
+rm -rf /home/$user/.ros/log/*
 
 # delete pilot logs
-rm -rf /home/$username/pilot/user/data/logs/*
+rm -rf /home/$user/pilot/user/data/logs/*
 
 # delete system logs
 rm -rf /var/log/*
 
 # this is needed for journalctl (systemd)
 mkdir /var/log/journal
+
+# clear bash history
+history -c
 
 exit 0
