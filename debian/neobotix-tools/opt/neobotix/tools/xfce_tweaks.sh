428366afca31c6f1abcea06d8aa31ec0804467a0
par <reiter@neobotix.de>
2023-01-26 11:44:32 +0100

    close #23; powermanagement, lockscreen and screensaver.

--- debian/neobotix-tools/opt/neobotix/tools/xfce_tweaks.sh
+++ debian/neobotix-tools/opt/neobotix/tools/xfce_tweaks.sh
@@ -8,17 +8,19 @@ xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false
 # Add keyboard language switcher to panel
 xfce4-panel --add=xkb
 
-# Disable lockscreen
+# Disable display power management
 xset s 0 0
 xset s off -dpms 
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false
+
+# Disable screensaver and lockscreen
+xfconf-query -c xfce4-screensaver -p /saver/enabled -t "bool" -s false -n
+xfconf-query -c xfce4-screensaver -p /lock/enabled -t "bool" -s false -n
 xfconf-query -c xfce4-session -p /shutdown/LockScreen -s false
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lock-screen-suspend-hibernate -s false
 
-# Disable power saving
-gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+# Showdown when power button is pressed
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/power-button-action -s 4
 
 # Disable saved sessions
 rm -rf /home/$user/.cache/sessions/* && chmod 500 /home/$user/.cache/sessions
