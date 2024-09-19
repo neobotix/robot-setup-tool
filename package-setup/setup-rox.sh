5311bcd0c7df1c84ee35d8b09b87b19f75ca6ed8
Pradheep Krishna <padhupradheep@gmail.com>
2024-09-19 17:37:08 +0200

    Update setup-rox.sh

--- package-setup/setup-rox.sh
+++ package-setup/setup-rox.sh
@@ -100,7 +100,7 @@ git clone --branch master          https://github.com/neobotix/neo_srvs2
 if [ "$kinematics" == "argo" ]; then
 	git clone --branch main https://github.com/neobotix/rox_argo_kinematics.git
 elif [ "$kinematics" == "diff" ]; then
-	git clone --branch $ROS_DISTRO https://github.com/neobotix/neo_kinematics_differential2.git
+	git clone --branch main https://github.com/neobotix/rox_diff_kinematics.git
 fi
 
 
