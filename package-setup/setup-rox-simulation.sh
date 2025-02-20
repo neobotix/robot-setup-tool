9e8033196c3a26f7316e6c0c3a7a2c192455e7f6
Pradheep Krishna <padhupradheep@gmail.com>
2025-02-20 13:41:06 +0100

    Update setup-rox-simulation.sh

--- package-setup/setup-rox-simulation.sh
+++ package-setup/setup-rox-simulation.sh
@@ -2,27 +2,73 @@
 
 # exit if any command below fails
 set -e
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+WHITE='\033[0;37m'
+
+echo "Welcome to the support assistant for setting up the ROX Simulation packages"
+echo "The workspace will be created in the /home/USER/ folder. In the future, we will add the option, to create the workspace in any desired directory"
 
 # Check if ROS is sourced
+empty_ros_distro=true
+ros_distros=" "
+if [ "$ROS_DISTRO" == "" ]; then
+	echo "ROS Distro is not sourced."
+    for dir in /opt/ros/*/; do
+        if [ "$(ls -A "$dir")" ]; then
+            empty_ros_distro=false
+            echo -e "$GREEN"
+            echo "  " $(basename "$dir")
+        fi
+    done
 
-if [ "$ROS_DISTRO" == "" ];
-then
-	echo "Installation cannot continue. No ROS sourced, please check if ROS is installed and sourced. Please try again after that!"
-	exit 0
+    if [ $empty_ros_distro == true ]; then
+        echo "ROS 2 is not installed at all"
+        echo "Please continue the installation once ROS 2 is installed"
+        echo "Abort"
+    else
+        echo -e "${RED} Please source any one of the ROS Distros above and try again later"
+        echo -e "${RED} Example: source /opt/ros/YOUR_DISTRO/setup.bash"
+        echo "Abort"
+        exit 0
+    fi
 fi
 
-if [ "$ROS_DISTRO" == "Humble" ];
-then
-	echo "Installation cannot continue. ROX Simulation is only supported in Iron and Jazzy"
-	exit 0
+# Check if the client ws is already there in the home folder
+cd ~/
+is_ws_installed=""
+
+if [ -d "${ROS_DISTRO}_ws" ]; then
+    echo "${ROS_DISTRO}_ws already exists."
+    echo -n "Do you want to delete it before continuing? (Y/n)"
+    read is_ws_installed
+
+    if [[ "$is_ws_installed" == "y" || "$is_ws_installed" == "Y" ]]; then
+        echo -e "  Deleting.."
+        rm -rf ${ROS_DISTRO}_ws
+        echo -e "  ${ROS_DISTRO}_ws has been deleted"
+    else
+        echo "Abort"
+        exit 0
+    fi
 fi
 
-echo "Welcome to the setup of the ROX simulation ws"
+uni_ans=""
+robot_model=""
 
-skip_depend="phidgets-drivers ur_client_library ur_msgs ur_description ur_robot_driver"
+# Go to home directory
+cd ~
+
+skip_depend="phidgets-drivers ur_client_library ur_msgs ur_robot_driver"
 
 # Install build tool
+echo "Installing colcon extensions"
 sudo apt install python3-colcon-common-extensions
+
+# Installing CycloneDDS
+echo "Installing CycloneDDS"
+sudo apt install ros-$ROS_DISTRO-rmw-cyclonedds-cpp
 
 #Install xterm
 
@@ -38,7 +84,6 @@ cd "$directory"
 
 # clone git repos here...
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/rox.git
-git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_nav2_bringup.git
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_local_planner2.git
 git clone --branch $ROS_DISTRO     https://github.com/neobotix/neo_localization2.git
 git clone --branch master          https://github.com/neobotix/neo_common2
@@ -60,8 +105,5 @@ echo "export LC_NUMERIC="en_US.UTF-8" " >> ~/.bashrc
 echo "source ~/"$directory_root"/install/setup.bash" >> ~/.bashrc
 
 echo "Installation successful !!!"
-
-# Temporarily setting up the GZ_RESOURCE_PATH from here
-echo "export GZ_SIM_RESOURCE_PATH=~/"$directory"/rox/:~/"$directory"/neo_gz_worlds/models" >> ~/.bashrc
 
 exit 0
