f94bad1fd69c2f2f4dfbaef20fa2ec3f97a7f1a5
Pradheep Krishna <padhupradheep@gmail.com>
2025-02-20 15:23:28 +0100

    Update setup-rox.sh

--- package-setup/setup-rox.sh
+++ package-setup/setup-rox.sh
@@ -23,7 +23,7 @@ echo "Welcome to the setup of your ROX robot, please select the dependencies tha
 uni_ans=""
 phi_ans=""
 realsense_ans=""
-skip_depend="ros_gz "
+skip_depend="ros_gz ros_gz_sim ros_gz_bridge gz_ros2_control "
 arm_type=" "
 use_imu="False"
 use_d435="False"
