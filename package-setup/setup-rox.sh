f51983dcfb04aaeb62ad4bfdb36fbcc290cf1e57
Pradheep Krishna <padhupradheep@gmail.com>
2026-05-12 15:38:56 +0200

    Change kinematics git clone branches to ROS_DISTRO
    Update branch names for kinematics repositories to use ROS_DISTRO.

--- package-setup/setup-rox.sh
+++ package-setup/setup-rox.sh
@@ -152,9 +152,9 @@ git clone --branch master          https://github.com/neobotix/neo_srvs2
 git clone --branch ros2            https://github.com/neobotix/joystick_drivers
 
 if [ "$kinematics" == "argo" ]; then
-	git clone --branch main https://github.com/neobotix/rox_argo_kinematics.git
+	git clone --branch $ROS_DISTRO https://github.com/neobotix/rox_argo_kinematics.git
 elif [ "$kinematics" == "diff" ]; then
-	git clone --branch main https://github.com/neobotix/rox_diff_kinematics.git
+	git clone --branch $ROS_DISTRO https://github.com/neobotix/rox_diff_kinematics.git
 fi
 
 if [ "$uni_ans" == "y" ]; then
